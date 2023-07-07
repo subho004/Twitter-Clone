faddb1ecea86090c33262c1ec3402ee0487d7688
AbhipreyaGhosh <ghoshabhipreya@gmail.com>
2023-07-08 03:38:37 +0530

    did some chnages

--- database.js
+++ database.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 // mongoose.set('useNewUrlParser', true);
 // mongoose.set('useUnifiedTopology', true);
 // mongoose.set('useFindAndModify', false);
 // mongoose.set('useUnifiedTopology', true);
 
+dotenv.config();
+
 class Database {
+  constructor() {
+    this.connect();
+  }
 
-    constructor() {
-        this.connect();
-    }
-
-    connect() {
-        mongoose.connect("mongodb+srv://admin:dbUserPassword@twitterclonecluster.e4ktagm.mongodb.net/?retryWrites=true&w=majority")
-            .then(() => {
-                console.log("database connection successful");
-            })
-            .catch((err) => {
-                console.log("database connection error " + err);
-            })
-    }
+  connect() {
+    mongoose
+      .connect(process.env.MONGO_URL)
+      .then(() => {
+        console.log("database connection successful");
+      })
+      .catch((err) => {
+        console.log("database connection error " + err);
+      });
+  }
 }
 
 module.exports = new Database();
